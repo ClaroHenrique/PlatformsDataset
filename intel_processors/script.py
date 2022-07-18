328cd1ff51a178ddfec2cb84b6f63437a3df2c27
Claro Henrique <claro.sales@reserva.ink>
2022-07-17 23:34:05 -0300

    add script intel processor data

--- intel_processors/script.py
+++ intel_processors/script.py
@@ -2,30 +2,38 @@ import requests
 from bs4 import BeautifulSoup as BS
 import time
 import json
+import os
 
-def get_html(url):
-  time.sleep(2)
+def get_html(url, delay=1):
+  time.sleep(1)
   return requests.get(url).text
 
 def search_initial_menu():
-  initial_menu_url = 'https://ark.intel.com/content/www/us/en/ark.html#@Processors'
-  print('%'*21, initial_menu_url, '%'*12)
-  html = get_html(initial_menu_url)
+  print("starting search")
+  html = get_html('https://ark.intel.com/content/www/us/en/ark.html#@Processors')
   soup = BS(html, 'html.parser')
-  n = len(soup.find_all('a', {'class': 'ark-accessible-color'}))
-
-  for proc_family in soup.find_all('a', {'class': 'ark-accessible-color'}):
+  page_content = soup.find('div', {'class': 'product-browse-div'}).find_all('a', {'class': 'ark-accessible-color'})
+  
+  for i, proc_family in enumerate(page_content):
+    print("progress:", i, '/', len(page_content))
+    #if i == 3: break
     href = proc_family.get('href') #/content/www/us/en/ark/products/series/98456/intel-100-series-desktop-chipsets.html
     if href == None: continue  
     suffix = href.split('/content/www/us/en/')[1]
-    sub_menu_url = 'https://ark.intel.com/content/www/us/en/' + str(suffix)
-    search_family_page(sub_menu_url)
+    family_page_url = 'https://ark.intel.com/content/www/us/en/' + str(suffix)
+    search_family_page(family_page_url)
 
 def search_family_page(family_menu_url):
-  print('$'*12, family_menu_url, '$'*12)
+  print('searching family in', family_menu_url)
 
   html = get_html(family_menu_url)
   soup = BS(html, 'html.parser')
+
+  content = soup.find('tbody')
+  if content == None: return
+  content = soup.find('tbody').find_all('a')
+  if content == None: return
+  #some family pages are empty
 
   for proc_info in soup.find('tbody').find_all('a'):
     href = proc_info.get('href') #/content/www/us/en/ark/products/series/98456/intel-100-series-desktop-chipsets.html
@@ -33,31 +41,27 @@ def search_family_page(family_menu_url):
     proc_page_url = 'https://ark.intel.com/content/www/us/en/' + str(suffix)
     search_processor_page(proc_page_url)
 
-
-#search_family_page('https://ark.intel.com/content/www/us/en/ark/products/series/217839/12th-generation-intel-core-i9-processors.html')
-
 processors_info = []
-
 def search_processor_page(processor_page_url):
-  print('_'*23, processor_page_url, '_'*23)
+  print('processing page', processor_page_url)
+  try:
+    html = get_html(processor_page_url)
+    soup = BS(html, 'html.parser')
 
-  html = get_html(processor_page_url)
-  soup = BS(html, 'html.parser')
-  proc_info = {}
+    proc_info = {}
+    proc_info["web_page_url"] = processor_page_url
 
-  for section in soup.find_all('div', {'class': 'blade-inside'}):
-    for list_item in section.find_all('li'):
-      label = list_item.find('span', {'class': 'label'}).text.strip()
-      value = list_item.find('span', {'class': 'value'}).text.strip()
-      proc_info[label] = value
-      print(label)
-      print(value)
-  
-  processors_info.append(proc_info)
+    for section in soup.find_all('div', {'class': 'blade-inside'}):
+      for list_item in section.find_all('li'):
+        label = list_item.find('span', {'class': 'label'}).text.strip()
+        value = list_item.find('span', {'class': 'value'}).text.strip()
+        proc_info[label] = value
+    processors_info.append(proc_info)
+  except:
+    print("error in", processor_page_url)
 
-print('-'*50)
-#search_processor_page('https://ark.intel.com/content/www/us/en/ark/products/228439/intel-core-i912950hx-processor-30m-cache-up-to-5-00-ghz.html')
 search_initial_menu()
 
-with open("intel_processors_data.txt", "w") as f:
+script_dir = os.path.dirname(__file__)
+with open(script_dir + "intel_processors_data.txt", "w") as f:
   f.write(json.dumps(processors_info, ensure_ascii=False, indent=2))
