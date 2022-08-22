0d5533f6408db2bcf4667b964583a8421ee3afc8
Claro Henrique <claro.sales@reserva.ink>
2022-08-22 10:47:25 -0300

    update script

--- techpowerup_gpu/get_info_per_html_script.py
+++ techpowerup_gpu/get_info_per_html_script.py
@@ -12,7 +12,16 @@ def search_product_page(html, url, access_date):
   product_info = {}
   product_info['web_page_url'] = url
   product_info['web_page_access_date'] = access_date
+  
+  # general specs
+  features = {}
+  for spec in soup.find_all('div', {'class': 'gpudb-specs-large__entry'}):
+    title = spec.find('dt').text.strip()
+    value = spec.find('dd').text.strip()
+    features[title] = value
+  product_info["General Specs"] = features
 
+  # specifics specs 
   for section in soup.find_all('section', {'class': 'details'}):
     section_title = section.find('h2').text.strip()
     features = {}
@@ -29,11 +38,10 @@ with open('product_html_data.json') as f:
 
 for html_info in product_html_data.values():
   search_product_page(html_info["html"], html_info["url"], html_info["access_date"])
-  break
 
-print('Result:::')
-print(products_info)
+#print('Result:::')
+#print(products_info)
 
 script_dir = os.path.dirname(__file__)
-with open(script_dir + "gpu_from_techpowerup_data.txt", "w") as f:
+with open(script_dir + "gpu_info.json", "w") as f:
   f.write(json.dumps(products_info, ensure_ascii=False, indent=2))
